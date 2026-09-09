f9e9b54eac514dd005a89dca6195180187d522d3
Musman Shahid <musmanshahid003@gmail.com>
2026-09-09 13:46:37 +0500

    remove the frontend folder and keep the ts frontend

--- frontend/src/hooks/queries/useHostelQueries.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from'@tanstack/react-query';
-import { hostelApi } from'../../api/endpoints/hostel.api';
-
-export const useMyHostel = () => {
- return useQuery({
- queryKey: ['myHostel'],
- queryFn: hostelApi.getMyHostel,
- });
-};
